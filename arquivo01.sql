1c750431ef33df0329fe7c5de6eac71d6bad54ff
Adilson Bortoloso <adilsonbortoloso@gmail.com>
2018-09-07 18:59:02 -0300

    commit do arquivo 01

--- arquivo01.sql
+++ arquivo01.sql
@@ -2,10 +2,6 @@ arquivo 01
 este é o arquivo principal
 e aqui tbm aqui coloquei uma alteração
 
-<<<<<<< HEAD
-Aqui tem mais algumas informações
-=======
-agora alterei esta linha mais com algumas informações
->>>>>>> 16760d6fb47eddc2cc78c86efe7a554c76756c31
+Arquivo 01
 
 aqui alterei na versão 01
